738f50bbe615238820d658dc12ab24b02a6e1036
Your Name <you@example.com>
2021-12-18 14:07:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+    
+      const amountToBeReturned = cashGiven.value - billAmount.value; 
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage("Do you wanna wash plates?");
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+    
+  // go over all the available
+  for (let i = 0; i < availableNotes.length; i++) {
+    // no of notes need for the denomination
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+
+    // amount left after calculating the number of notes needed
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+
+    // updating the no of notes in the table for the current amount
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
